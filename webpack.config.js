48c45a81300cdac8f93699a525d3dcc43a07045e
Akshay Kadam <akshaykadam0904@gmail.com>
2016-09-28 23:54:15 +0530

    Adding script on Wed Sep 28 23:54:15 IST 2016 :blush:

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
   },
   plugins: [HTMLWebpackPluginConfig,
             new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+            'process.env.NODE_ENV': JSON.stringify('production')
             }),
             new webpack.optimize.UglifyJsPlugin({
               compress: { warnings: false },
